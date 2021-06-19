d74a20b7dd573f7535c0582d411665f27c120724
keitasuzukijp <hello@keitasuzuki.jp>
2021-06-19 10:45:47 +0900

    内容更新
    すみません、取り急ぎ直接GitHub上のデータを直接更新いたします。

--- pages/parent.vue
+++ pages/parent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Parent">
     <h2 class="Parent-Heading">
-      臨時休校中の新型コロナウイルス感染症対応についてのお願い
+      お子様をお持ちの皆様へ
     </h2>
     <div v-for="(item, i) in items" :key="i">
       <TextCard :title="item.title" :body="item.body" />
@@ -18,27 +18,27 @@ export default {
     return {
       items: [
         {
-          title: '1 感染予防・健康管理',
+          title: '感染予防・健康管理',
           body:
             '〇  不特定多数の人の集まる場所等への外出を避け、基本的に自宅で過ごしてください。<br />〇  手洗い、咳エチケット等により、感染予防に努めてください。<br />　<a href="https://www.pref.yamanashi.jp/koucho/coronavirus/documents/tadasiitearai_1.pdf" target="_blank" rel="noopener">【参考】正しい手の洗い方（PDF：357KB）</a> <br />〇  規則正しい生活を心がけ、日常の健康管理に十分気を付けてください。<br />　<a href="https://www.pref.yamanashi.jp/koucho/coronavirus/documents/20200306newcoronabousi.pdf" target="_blank" rel="noopener">新型コロナウイルスを防ぐには（PDF：250KB）</a>'
         },
         {
-          title: '2 感染症を疑う場合の対応',
+          title: '感染症を疑う場合の対応',
           body:
-            '〇  風邪の症状や、37.5度以上の発熱が４日以上続いている、強いだるさ（倦怠感）、息苦しさ（呼吸困難）がある場合は、各保健所にご相談ください。<br /><a href="https://www.pref.yamanashi.jp/koucho/coronavirus/info_coronavirus_consultation.html#Covid19_GeneralConsultation" target="_blank" rel="noopener">〇  新型コロナウイルス感染症専用相談ダイヤル</a>'
+            '〇 息苦しさ（呼吸困難）、強いだるさ（倦怠感）、高熱等の強い症状がある場合、または発熱や咳などの比較的軽い風邪の症状が続いている場合は、各窓口にご相談ください。<br />各窓口の電話番号等については「新型コロナウイルス感染症に関する相談窓口等」（山梨県）をご確認ください。<br />「<a href="https://www.pref.yamanashi.jp/koucho/coronavirus/info_coronavirus_consultation.html">新型コロナウイルス感染症に関する相談窓口等</a>」（山梨県）'
         },
         {
-          title: '3 学校の臨時休校について',
+          title: '子育て世帯生活支援特別給付金（ひとり親世帯分）について',
           body:
-            '〇  自治体により異なるため、詳細は各学校からのお知らせ等をご確認ください。'
+            '〇  新型コロナウイルス感染症の影響を受けて損害を受けた低所得のひとり親世帯を見舞う観点から、子育て世帯生活支援特別給付金を支給します。<br /><a href="https://www.pref.yamanashi.jp/kodomo-fukushi/kosodatesetai-kyufukin_hitorioya.html" target="_blank" rel="noopener">https://www.pref.yamanashi.jp/kodomo-fukushi/kosodatesetai-kyufukin_hitorioya.html</a>'
         },
         {
-          title: '4 子育て家庭休業助成金制度について',
+          title: '【終了しました】子育て家庭休業助成金制度について',
           body:
-            '〇  子どもが小学校や保育所等へ登校・登園をしなかったことに伴い保護者が休業を余儀なくされた方に、一定額の助成を行うものです。<br /><a href="https://www.pref.yamanashi.jp/koucho/coronavirus/info_coronavirus_support.html#person" target="_blank" rel="noopener">https://www.pref.yamanashi.jp/koucho/coronavirus/info_coronavirus.html#josei</a>'
+            '〇  子どもが小学校や保育所等へ登校・登園をしなかったことに伴い保護者が休業を余儀なくされた方に、一定額の助成を行うものです。<br /><a href="https://www.pref.yamanashi.jp/kodomo-fukushi/coronavirus_joseikin.html" target="_blank" rel="noopener">https://www.pref.yamanashi.jp/kodomo-fukushi/coronavirus_joseikin.html</a>'
         },
         {
-          title: '5 子供の居場所づくり等に関する取り組みについて',
+          title: '子供の居場所づくり等に関する取り組みについて',
           body:
             '〇  山梨県教育委員会による取り組みの紹介です。<br /><a href="https://www.pref.yamanashi.jp/kyouikusom/20200227.html" target="_blank" rel="noopener">子供の居場所づくり等に関する取り組みについて</a>'
         }
